78fa4412146fecbbb3d9ab7ffbf7bfc0bf1cbf4a
thehxdev <hossein.khosravi.ce@gmail.com>
2025-08-15 22:30:30 +0330

    removed unused scc_manager type
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- src/ssc.h
+++ src/ssc.h
@@ -17,14 +17,6 @@ struct ssc_config {
     uint16_t remote_port;
 };
 
-typedef struct ssc_manager {
-    ssc_crypto_cipher *cipher;
-    struct ssc_config config;
-    // struct sockaddr remaddr, lisaddr;
-    unsigned char key[AES_MAX_KEY_SIZE+1];
-    unsigned char keysize;
-} ssc_manager_t;
-
 int ssc_config_readall(arena_t *arena, const char *dlpath, struct ssc_config *config);
 
 #endif // _SSC_H_
