585e35a4978cb876abbae32e510b00a0337d6a19
thehxdev <hossein.khosravi.ce@gmail.com>
2025-08-15 15:54:43 +0330

    add -DSSC_OS_UNIX to compiler flags
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- build.bash
+++ build.bash
@@ -26,7 +26,9 @@ log_run $CC -fPIC -shared -o "$BUILD_PATH/config.so" "$SELF_PATH/config.c"
 # build ssc-local
 log_run $CC \
     "${CFLAGS[@]}" "${LDFLAGS[@]}" \
+    -DSSC_OS_UNIX=1 \
     -I"$DEPS_PATH/include" \
+    -I"$SELF_PATH/src" \
     -L"$DEPS_PATH/lib" \
     -o "$BUILD_PATH/ssc-local" \
     "$SRC_PATH/local_build.c" \
