23bd8d359f9686374cddd2edb57b79de2bf5c874
thehxdev <hossein.khosravi.ce@gmail.com>
2026-01-07 21:42:19 +0330

    added shell.nix
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+    inherit (pkgs) mkShell;
+    inherit (pkgs.lib) makeLibraryPath;
+
+    dependencyLibraries = with pkgs; [
+        libuv
+        libblake3
+        openssl
+    ];
+in
+    mkShell {
+        nativeBuildInputs = with pkgs; [
+            gcc
+            gnumake
+            cmake
+        ];
+
+        buildInputs = dependencyLibraries;
+
+        packages = with pkgs; [
+            gdb
+        ];
+
+        shellHook = ''
+        export CC=gcc
+        export CXX=g++
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ makeLibraryPath dependencyLibraries }
+        '';
+    }
